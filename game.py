5363306eb724a4dbd2a9c1f16b67bf28639d743b
ryanflorance-dev <ryan.florance@my.rongotai.school.nz>
2026-06-29 01:01:47 +0000

    main_menu complete game start

--- game.py
+++ game.py
@@ -1,15 +1,16 @@
-start = 0
 def mainmenu():
     print(f"Welcome")
     print("1: start game")
     print("2: quit")
+    global start
+    start = 0
     option = int(input())
     if option == 1:
-        start + 1 == start
-        print(f"{start}")
+        start += 1
         return start
     elif option == 2:
-        pass
+        start += -1
+        return start
     else:
         print("Error, must pick an actualy option. Reason for this error may be one of the following:")
         print("Number you selected is more than 2,")
@@ -17,3 +18,13 @@ def mainmenu():
         print("Number you selected is a decimal,")
         print("You wrote out the number instead of using the digit.")
 
+def game():
+    pass
+
+mainmenu()
+if start == 1:
+    game()
+elif start == -1:
+    pass
+else:
+    print("Error")
